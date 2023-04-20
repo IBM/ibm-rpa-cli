621c96d505f60246b55a09d9c5beb5c95b8b62d7
JobaDiniz <jobertodinizjunior@gmail.com>
2023-04-20 16:38:50 -0300

    test(environment): new env tests

--- src/Joba.IBM.RPA.Cli/Environment/EnvironmentCommand.New.cs
+++ src/Joba.IBM.RPA.Cli/Environment/EnvironmentCommand.New.cs
@@ -39,23 +39,43 @@ namespace Joba.IBM.RPA.Cli
             public async Task HandleAsync(RemoteOptions options, ILogger logger,
                 IRpaClientFactory clientFactory, IProject project, InvocationContext context)
             {
-                project.EnsureCanConfigure(options.Alias);
+                var handler = new NewEnvironmentHandler(logger, project, context.Console, clientFactory);
+                await handler.HandleAsync(options, context.GetCancellationToken());
+            }
 
-                var cancellation = context.GetCancellationToken();
-                var regionSelector = new RegionSelector(context.Console, clientFactory, project);
-                var region = await regionSelector.SelectAsync(options.Address, options.RegionName, cancellation);
+            internal class NewEnvironmentHandler
+            {
+                private readonly ILogger logger;
+                private readonly IProject project;
+                private readonly IConsole console;
+                private readonly IRpaClientFactory clientFactory;
 
-                using var client = clientFactory.CreateFromRegion(region);
-                var accountSelector = new AccountSelector(context.Console, client.Account);
-                var credentials = await accountSelector.SelectAsync(options.UserName, options.TenantCode,options.Password, cancellation);
-                var environment = await project.ConfigureEnvironment(client.Account, options.Alias, region, credentials, cancellation);
-                await project.SaveAsync(cancellation);
+                internal NewEnvironmentHandler(ILogger logger, IProject project, IConsole console, IRpaClientFactory clientFactory)
+                {
+                    this.logger = logger;
+                    this.project = project;
+                    this.console = console;
+                    this.clientFactory = clientFactory;
+                }
 
-                logger.LogInformation("Hi '{PersonName}', the following environment has been configured: {Environment}\n" +
-                    "Use '{RpaCommandName} {PullCommand} [asset name] --env {Alias}' to pull files from the environment.\n" +
-                    "Use '{RpaCommandName} {DeployCommand} {Environment}' to deploy the project to the environment.",
-                    environment.Session.Current.PersonName, environment, RpaCommand.CommandName, PullCommand.CommandName, environment.Alias,
-                    RpaCommand.CommandName, DeployCommand.CommandName, environment.Alias);
+                internal async Task HandleAsync(RemoteOptions options, CancellationToken cancellation)
+                {
+                    project.EnsureCanConfigure(options.Alias);
+                    var regionSelector = new RegionSelector(console, clientFactory, project);
+                    var region = await regionSelector.SelectAsync(options.Address, options.RegionName, cancellation);
+
+                    using var client = clientFactory.CreateFromRegion(region);
+                    var accountSelector = new AccountSelector(console, client.Account);
+                    var credentials = await accountSelector.SelectAsync(options.UserName, options.TenantCode, options.Password, cancellation);
+                    var environment = await project.ConfigureEnvironment(client.Account, options.Alias, region, credentials, cancellation);
+                    await project.SaveAsync(cancellation);
+
+                    logger.LogInformation("Hi '{PersonName}', the following environment has been configured: {Environment}\n" +
+                        "Use '{RpaCommandName} {PullCommand} [asset name] --env {Alias}' to pull files from the environment.\n" +
+                        "Use '{RpaCommandName} {DeployCommand} {Environment}' to deploy the project to the environment.",
+                        environment.Session.Current.PersonName, environment, RpaCommand.CommandName, PullCommand.CommandName, environment.Alias,
+                        RpaCommand.CommandName, DeployCommand.CommandName, environment.Alias);
+                }
             }
         }
     }
